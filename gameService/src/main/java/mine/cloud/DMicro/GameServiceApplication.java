91391ef76a549180801e85d95c80bf566d9f8641
v-lijj00 <tb963472217@aol.com>
2023-12-12 23:30:58 +0800

    2023-12-12 1. 新增游戏模块

--- gameService/src/main/java/mine/cloud/DMicro/GameServiceApplication.java
+++ gameService/src/main/java/mine/cloud/DMicro/GameServiceApplication.java
@@ -1,4 +1,13 @@
 package mine.cloud.DMicro;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("mine.cloud.DMicro.dao")
+@SpringBootApplication
 public class GameServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GameServiceApplication.class,args);
+    }
 }
